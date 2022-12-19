99f8b764f9b64426383e487d90738e882789878c
irkham <irkham.nurhadi.13@gmail.com>
2022-12-19 15:02:06 +0700

    add dotenv

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import apiRouter from './routes/api.js';
 import connection from './connection.js';
+import dotenv from 'dotenv';
 
+const env = dotenv.config().parsed;
 const app = express();
 
 app.use(express.json());
@@ -14,6 +16,6 @@ app.use((req, res) => {
 
 connection();
 
-app.listen(5000, () => {
-  console.log('Server started on port 5000');
+app.listen(env.APP_PORT, () => {
+  console.log(`Server started on port ${env.APP_PORT}`);
 });
