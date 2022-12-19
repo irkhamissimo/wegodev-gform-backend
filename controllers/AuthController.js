3d9f6146ac090137e510bdaae1c87f21379dfd69
irkham <irkham.nurhadi.13@gmail.com>
2022-12-19 16:52:36 +0700

    throw error 400

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,6 +3,10 @@ import User from '../models/User.js';
 class AuthController {
   async register(req, res) {
     try {
+      if (!req.body.fullname) throw {code: 400, message: 'FULLNAME IS REQUIRED!'}
+      if (!req.body.email) throw {code: 400, message: 'EMAIL IS REQUIRED!'}
+      if (!req.body.password) throw {code: 400, message: 'PASSWORD IS REQUIRED!'}
+      
       const user = await User.create(req.body);
       if (!user) throw { code: 500, message: 'USER_REGISTER_FAILED' };
       return res
