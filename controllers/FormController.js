645a3d5265c96304b0147b10a16f83678dc7beea
irkham <irkham.nurhadi.13@gmail.com>
2022-12-31 21:52:08 +0700

    show and update form

--- controllers/FormController.js
+++ controllers/FormController.js
@@ -1,17 +1,15 @@
 import mongoose from 'mongoose';
 import Form from '../models/Form.js';
 
-class FormController { 
+class FormController {
   async store(req, res) {
     try {
-      console.log(req.jwt)
       const form = await Form.create({
         userId: req.jwt.payload.id,
         title: 'Untitled Form',
         description: null,
         public: true,
       });
-      
 
       if (!form) {
         throw { code: 500, message: 'FAILED_CREATE_FORM' };
@@ -20,7 +18,65 @@ class FormController {
         .status(200)
         .json({ status: true, message: 'SUCCESS_CREATE_FORM', form });
     } catch (error) {
-      console.log(error)
+      console.log(error);
+      return res
+        .status(error.code || 500)
+        .json({ status: false, message: error.message });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: 'FORM_ID_REQUIRED' };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: 'Invalid ID' };
+      }
+
+      const form = await Form.findOne({
+        _id: req.params.id,
+        userId: req.jwt.payload.id,
+      });
+
+      if (!form) {
+        throw { code: 404, message: 'Form not found' };
+      }
+      return res
+        .status(200)
+        .json({ status: true, message: 'SUCCESS_LOAD_FORM', form });
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(error.code || 500)
+        .json({ status: false, message: error.message });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: 'FORM_ID_REQUIRED' };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: 'Invalid ID' };
+      }
+      const form = await Form.findOneAndUpdate(
+        {
+          _id: req.params.id,
+          userId: req.jwt.payload.id,
+        },
+        req.body,
+        { new: true }
+      );
+
+      if (!form) {
+        throw { code: 500, message: 'FAILED_UPDATE_FORM' };
+      }
+      return res
+        .status(200)
+        .json({ status: true, message: 'SUCCESS_UPDATE_FORM', form });
+    } catch (error) {
       return res
         .status(error.code || 500)
         .json({ status: false, message: error.message });
