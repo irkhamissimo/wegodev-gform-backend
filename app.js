0eccc7e4369b374775b428b5947db843775afc61
irkham <irkham.nurhadi.13@gmail.com>
2023-01-18 11:43:27 +0700

    add cors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ import apiRouter from './routes/api.js';
 import connection from './connection.js';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const env = dotenv.config().parsed;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: 'http://localhost:5454 ' }));
 app.use('/', apiRouter);
 
 app.use((req, res) => {
